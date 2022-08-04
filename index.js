9ea262c197045047dd7e66161bc4a223caba939a
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-04 12:39:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats=["Milo", "Otis","Garfield"]
+
+function destructivelyAppendCat(name){
+     return cats.push(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name)
+}
+function appendCat(name){
+    let cats2= ["Milo", "Otis","Garfield", name]
+    return cats2
+}
+function prependCat(name){
+    let cats2= [name, "Milo", "Otis","Garfield"]
+    return cats2
+}
+function removeLastCat(){
+    let cats2= cats.slice(0,-1)
+    return cats2
+}
+function removeFirstCat(){
+    let cats2= cats.slice(1)
+    return cats2
+}
